3fdb2cff17350910acddd7368f1d045502427224
Usman Rafi <usman.rafi.96@gmail.com>
2017-03-07 22:04:53 +0500

    Usman was here too

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -6,10 +6,10 @@ int main()
 
 	cout << "Chess";
 	cout << "Haroon was here";
-<<<<<<< HEAD
+
 	cout << "Usman was here too";
-=======
+
 	cout << "acha ji - hussam";
->>>>>>> f65bfc3f04dba33fd1fdca2fbfc48caaa8028073
+
 	return 0;
 }
