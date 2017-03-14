3db9abefdd8ec21e22acdc8d87f9516cc89ad18c
Haroon Ahmad <Haroon Ahmad>
2017-03-14 20:26:06 +0500

    Updated main

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "groupName.h"
+#include "chessState.h"
+#include "move.h"
 using namespace std;
 
-int main(){
-	cout << "Chess";
-	return 0;
+int main()
+{
+    cout << "Hello world!" << endl;
+    chessState game;
+    game.showState();
+    chessMove PlayerMove;
+
+    playerGROUP_NAME player(1);
+    int totalMoves = game.makeValidMovesList();
+    player.decideMoveUsingMINIMAX(game, PlayerMove, 3);
+    game.makeMove(PlayerMove);
+    game.showState();
+
+    return 0;
 }
