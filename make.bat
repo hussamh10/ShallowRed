d9ff5206aeca0f06d58ddcfe61d82e1324142536
Muhammad Haroon <m_haroon96@hotmail.com>
2017-03-21 22:12:14 +0500

    Added Erwin

--- make.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-cd Chess
-g++ -Wl,armadillo\lib_win64\blas_win64_MT.lib,armadillo\lib_win64\lapack_win64_MT.lib -Iarmadillo\include *.cpp -std=c++11 -o Chess.exe
-md ..\Out
-move Chess.exe ..\Out
-xcopy armadillo\lib_win64\*.dll ..\Out
